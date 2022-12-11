611d6abffbf60a1ba008e050bf9067e589dddeaa
georgep9 <g-patt@outlook.com>
2022-12-11 17:04:17 +1000

    rust init()

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,22 @@
+
+const CODE: [i32; 26] = [0, 2, 1, 2, 3, 4, 5, 6, 7, -1, 8, 9, 10, 11, -1, 12, 13, 14, 15, 16, 1, 17, 18, 5, 19, 3];
+fn encode(ch: char) -> i32 { CODE[(ch as u32 - 'A' as u32) as usize] }
+const LEN: u32 = 6;
+const AA_NUMBER: u32 = 20;
+const EPSILON: f32 = 1e-010;
+
+
+fn init(m: &mut u32, m1: &mut u32, m2: &mut u32){
+    *m2 = 1;
+    for _n in 0..LEN-2 {
+        *m2 = *m2 * AA_NUMBER; // M2 = AA_NUMBER ^ (LEN-2);
+    }
+    *m1 = *m2 * AA_NUMBER; // M1 = AA_NUMBER ^ (LEN-1);
+    *m = *m1 * AA_NUMBER; // M  = AA_NUMBER ^ (LEN);
+}
+        
 fn read_input_file() {
-    println!("read_input_file()");
+    println!("read_input_file");
 }
 
 fn load_all_bacteria_parallel() {
@@ -11,8 +28,18 @@ fn compare_all_bacteria_parallel() {
 }
 
 fn main() {
-    read_input_file()
-    load_all_bacteria_parallel()
-    compare_all_bacteria_parallel()
+    let mut _number_bacteria: u32;
+    let mut _bacteria_names: char;
+    let mut _m: u32 = 0;
+    let mut _m1: u32 = 0;
+    let mut _m2: u32 = 0;
+
+    init(&mut _m, &mut _m1, &mut _m2);
+    
+    println!("{} {} {}", &mut _m, &mut _m1, &mut _m2);
+
+    read_input_file();
+    load_all_bacteria_parallel();
+    compare_all_bacteria_parallel();
 }
 
